b82afd11201c15c2771d8e57f1c11a20f2a3733c
Ganzfriend <melissaganzfried@gmail.com>
2021-07-15 13:37:52 +0200

    set up homepage, add dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-const HomePage = () => {};
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.unsplash.com/photo-1531944213227-db53a6d0f3bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1482&q=80",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1542576329-818ff4ffda3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1487&q=80",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
 
 export default HomePage;
