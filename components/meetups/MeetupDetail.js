e5afd57a998cdd4d09e0d26a4d1aa1697f8230d7
Ganzfriend <melissaganzfried@gmail.com>
2021-07-15 17:52:51 +0200

    style meetup detail

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,11 +1,13 @@
+import classes from "./MeetupDetail.module.css";
+
 const MeetupDetail = ({ image, title, address, description }) => {
   return (
-    <Fragment>
+    <section className={classes.detail}>
       <img src={image} alt={title} />
       <h1>{title}</h1>
       <address>{address}</address>
       <p>{description}</p>
-    </Fragment>
+    </section>
   );
 };
 
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,8 @@
 // this folder name in brackets is another way to allow for dynamic nested pages
+// localhost:3000/<anything> will now lead to this endpoint
 import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-// localhost:3000/<anything> will now lead to this endpoint
 const MeetupDetails = () => {
   return (
     <MeetupDetail
